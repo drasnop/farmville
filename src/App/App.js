24116410d6fd485e2ccb7b75c1c54dad1d92b857
antoine <bycdmcyigbas@gmail.com>
2017-11-15 20:35:16 -0800

    "Load" data from local json files

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
+import farms from "../data/farms.json";
+import sites from "../data/sites.json";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // In a real app we would load these from API
+    this.state = {
+      farms: farms,
+      sites: sites
+    };
+  }
+
   render() {
-    return <div className="App">Hello World!</div>;
+    return (
+      <div className="App">
+        {this.state.farms[Object.keys(this.state.farms)[0]].name}
+      </div>
+    );
   }
 }
 
