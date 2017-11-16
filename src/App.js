6d9dfe9deaf634ecddda5070781ed096bc155ae4
antoine <bycdmcyigbas@gmail.com>
2017-11-15 19:42:18 -0800

    props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,14 +8,30 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title"> {this.props.text} </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code> src / App.js </code> and save to reload.
         </p>
+        <h1> Hello, {formatName(user)}! </h1>
       </div>
     );
   }
 }
+
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+const user = {
+  firstName: "Harper",
+  lastName: "Perez"
+};
+
+// const element = (
+//   <h1>
+//     Hello, {formatName(user)}!
+//   </h1>
+// );
 
 export default App;
